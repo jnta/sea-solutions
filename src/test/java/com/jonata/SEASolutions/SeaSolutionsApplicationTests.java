53b3ace8f1662c1dd3e39974cb859b40715b272d
Jonata <jonata.a@outlook.com>
2022-09-27 11:47:53 -0300

    First commit

---
+++ src/test/java/com/jonata/SEASolutions/SeaSolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonata.SEASolutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeaSolutionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
